877f1b40b1d15749984bf0491cb0fcc92a5ed233
ipinid613 <ipinid613@kakao.com>
2021-06-25 01:20:49 +0900

    21.06.25 / 2번문제 재풀이
    복습

--- 01_사칙연산.py
+++ 01_사칙연산.py
@@ -1,4 +1,13 @@
-a,b = map(int, input().split())
+#1회차
+# a,b = map(int, input().split())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+
+#2회차(21.06.24)
+a,b = map(int,input().split())
 print(a+b)
 print(a-b)
 print(a*b)
--- 02_곱셈.py
+++ 02_곱셈.py
@@ -1,8 +1,29 @@
 #세자리수 곱하기
 # a, b = map(str, input().split())
+# a = int(input())
+# b = input()
+# print(a * int(b[2]))
+# print(a * int(b[1]))
+# print(a * int(b[0]))
+# print(a * int(b))
+
+#2회차
 a = int(input())
 b = input()
-print(a * int(b[2]))
-print(a * int(b[1]))
-print(a * int(b[0]))
-print(a * int(b))
+a_result = a*(int(b[2]))
+b_result = a*(int(b[1]))
+c_result = a*(int(b[0]))
+d_result = a*int(b)
+print(a_result)
+print(b_result)
+print(c_result)
+print(d_result)
+
+#2회차 - 나머지 이용
+num1 = int(input())
+num2 = int(input()) #385
+
+print(num1 * (num2%10)) #5
+print(num1 * ((num2%100)//10)) #8
+print(num1 * (num2//100)) #3
+print(num1 * num2)
